3163c1651f82c31a394cdd204c0d2398b3ef53da
dAxeponb <dragonxxl2014@gmail.com>
2025-04-06 19:52:11 +0700

    Move color theme to folder colors and add lazy-lock.json

--- init.lua
+++ init.lua
@@ -11,5 +11,11 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins")
+require("lazy").setup({
+  { import = "plugins" },
+  { import = "plugins.colors" },
+  checker = {
+    enabled = false
+  }
+})
 
